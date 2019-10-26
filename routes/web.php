2d532ba1504eea86a3e66ca81770c60706283399
Abdalla Hisham <abdallahisham@yandex.com>
2019-10-26 02:36:26 +0200

    Fix web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-
 Route::get('/', function () {
     return redirect('/shop');
 });
@@ -11,8 +10,6 @@ Route::get('/shop', 'HomeController@index')->name('home');
 
 Route::get('/category/{category}/products/', 'CategoryProductsController@index');
 
-
-
 Route::get('/products/{product}', 'ProductsController@show');
 
 Route::post('/products/{product}/rate', 'ProductRatingController@store')->middleware('auth');
@@ -21,22 +18,14 @@ Route::post('/products/{product}/reviews', 'ProductReviewsController@store')->mi
 
 Route::delete('/products/{product}/reviews/{review}', 'ProductReviewsController@destroy')->middleware('auth');
 
-
 Route::get('/cart', 'CartController@index')->name('cart');
-
 
 Route::post('/cart/{product}', 'CartController@store');
 
-<<<<<<< HEAD
 Route::get('/checkout', 'CheckoutController@index')->name('checkout.index');
 Route::post('/checkout', 'CheckoutController@store')->name('checkout.store');
 Route::get('/checkout/{order}', 'CheckoutController@show')->name('checkout.show');
-=======
 Route::delete('/cart/{rowId}', 'CartController@destroy');
-
-Route::get('/checkout', 'CheckoutController@index');
->>>>>>> c4ea51f00ba62765b0d9a26aed7bf23db15e38f1
-
 Route::get('/contact-us', 'PagesController@contactUs')->name('contact us');
 
 Route::prefix('admin')
