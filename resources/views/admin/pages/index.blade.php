2577ea40871acdce7c65ed9bed8b8f40908060a2
Abdalla Hisham <abdallahisham@yandex.com>
2019-10-29 23:05:13 +0200

    Just to keep on]

--- resources/views/admin/pages/index.blade.php
+++ resources/views/admin/pages/index.blade.php
@@ -2,26 +2,38 @@
 
 
 @section('content')
-    <section>
-        <form action="{{ route('admin.pages.update') }}" method="post" class="form-horizontal col-lg-6">
-            @csrf
-            @method('patch')
-            @foreach($pages as $page)
-            <div class="form-group">
-                <label class="control-label col-sm-4" for="body_{{ $page->slug }}">{{ $page->title }}</label>
-                <div class="col-sm-8">
-                    <textarea name="body[{{ $page->slug }}]" id="body_{{ $page->slug }}"
-                              class="form-control" rows="8">{{ $page->body }}</textarea>
+    <div class="row">
+        <div class="col-lg-12">
+            <form action="{{ route('admin.pages.update') }}" method="post" class="form-horizontal col-lg-6">
+                @csrf
+                @method('patch')
+                @foreach($pages as $page)
+                    <div class="form-group">
+                        <label class="control-label col-sm-4" for="body_{{ $page->id }}">{{ $page->title }}</label>
+                        <div class="col-sm-8">
+                    <textarea name="body[{{ $page->slug }}]" id="body_{{ $page->id }}"
+                              rows="8" cols="150">{{ $page->body }}</textarea>
+                        </div>
+                    </div>
+                @endforeach
+                <div class="col-sm-8 col-sm-offset-4">
+                    <button type="submit" class="btn btn-primary">Update</button>
                 </div>
-            </div>
-            @endforeach
-            <div class="col-sm-8 col-sm-offset-4">
-                <button type="submit" class="btn btn-primary">Update</button>
-            </div>
-        </form>
-    </section>
+            </form>
+        </div>
+    </div>
 
 @endsection
 @section('css')
     <link rel="stylesheet" href="/css/admin.css">
+@stop
+
+@section('js')
+
+    <script src="https://cdn.ckeditor.com/4.13.0/standard/ckeditor.js"></script>
+    <script>
+        @foreach($pages as $page)
+            CKEDITOR.replace('body_{{ $page->id }}');
+        @endforeach
+    </script>
 @stop
--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.shop')
 
 @section('content')
-    <h1 class="text-center">@lang('general.' . $page->slug)</h1>
-    <section>
-        <p>
+    <div class="row">
+        <div class="col-lg-6 col-lg-offset-3">
+            <h1 class="text-center">@lang('general.' . $page->slug)</h1>
             {!! nl2br($page->body) !!}
-        </p>
-    </section>
+        </div>
+    </div>
 @endsection
