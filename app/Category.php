2cdb6b6481bca400befb4dd6314602b017466c34
owiesnama <37296520+owiesnama@users.noreply.github.com>
2020-03-08 23:25:00 +0200

    review the doc blocks

--- app/Category.php
+++ app/Category.php
@@ -6,19 +6,27 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 /**
+ * @property int $id
  * @property mixed $products
  * @property mixed $children
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
  * @mixin Builder
  * @method static Category main()
  */
 class Category extends Model
 {
+    /**
+     * spcify fillable items on this category model
+     *
+     * @var array
+     */
     public $fillable = [
         'name', 'desc', 'parent_id', 'status', 'code'
     ];
 
     /**
-     * Get parent category of this category
+     * Get parent category of this category.
      *
      * @return $this
      */
@@ -31,7 +39,7 @@ class Category extends Model
     }
 
     /**
-     * Get associated children
+     * Get associated children.
      *
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
@@ -40,6 +48,11 @@ class Category extends Model
         return $this->hasMany(Category::class, 'parent_id');
     }
 
+    /**
+     * Get all category that has the same parent category.
+     *
+     * @return \Illuminate\Database\Eloquent\Collection|static[]
+     */
     public function siblings()
     {
         return $this->where('parent_id', $this->parent->id)->get();
--- app/OrderDetails.php
+++ app/OrderDetails.php
@@ -6,13 +6,31 @@ use Illuminate\Contracts\Support\Arrayable;
 
 class OrderDetails implements Arrayable
 {
-
+    /**
+     * @var string
+     */
     public $name;
+    /**
+     * @var string
+     */
     public $city;
+    /**
+     * @var string
+     */
     public $address;
+    /**
+     * @var string
+     */
     public $phone;
+    /**
+     * @var string|json
+     */
     public $details;
 
+    /**
+     * OrderDetails constructor.
+     * @param array $details
+     */
     public function __construct(array $details)
     {
         $this->name = $details['name'];
@@ -22,6 +40,111 @@ class OrderDetails implements Arrayable
         $this->description = $details['details'];
     }
 
+    /**
+     * get the name of the buyer
+     *
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * set the name of the buyer
+     *
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * get the city of the buyer
+     *
+     * @return mixed
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * set the city of the buyer
+     *
+     * @param mixed $city
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+    }
+
+    /**
+     * get the address of the buyer
+     *
+     * @return mixed
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * set the address of the buyer
+     *
+     * @param mixed $address
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+
+    /**
+     * get the buyer phone
+     *
+     * @return mixed
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * set buyer of the order phone
+     *
+     * @param mixed $phone
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+    }
+
+    /**
+     * get the description of the order
+     *
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * set the description of the order
+     *
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * specify what to return when serialize to object
+     *
+     * @return array
+     */
     public function toArray()
     {
         return [
@@ -31,85 +154,5 @@ class OrderDetails implements Arrayable
             'phone' => $this->phone,
             'details' => $this->details,
         ];
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    /**
-     * @param mixed $name
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getCity()
-    {
-        return $this->city;
-    }
-
-    /**
-     * @param mixed $city
-     */
-    public function setCity($city)
-    {
-        $this->city = $city;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getAddress()
-    {
-        return $this->address;
-    }
-
-    /**
-     * @param mixed $address
-     */
-    public function setAddress($address)
-    {
-        $this->address = $address;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getPhone()
-    {
-        return $this->phone;
-    }
-
-    /**
-     * @param mixed $phone
-     */
-    public function setPhone($phone)
-    {
-        $this->phone = $phone;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getDescription()
-    {
-        return $this->description;
-    }
-
-    /**
-     * @param mixed $description
-     */
-    public function setDescription($description)
-    {
-        $this->description = $description;
     }
 }
