3992b6062804426888db2225c2e990c019b8af7a
owiesnama <37296520+owiesnama@users.noreply.github.com>
2020-03-09 13:10:00 +0200

    show message if there is now recent products
    In case of there is no products on the shop show a message for better UX

--- resources/views/partials/recent-products.blade.php
+++ resources/views/partials/recent-products.blade.php
@@ -1,11 +1,15 @@
 <div class="row">
     <div class="product__wrap clearfix">
 
-        @foreach($recentProducts as $recentProduct)
+        @forelse($recentProducts as $recentProduct)
             <div class="col-md-3 col-lg-3 col-sm-6 col-xs-12">
                 <Product :product="{{$recentProduct}}"></Product>
             </div>
-        @endforeach
+        @empty
+            <div class="flex justify-center text-muted">
+                There is no products right now
+            </div>
+        @endforelse
 
     </div>
 </div>
